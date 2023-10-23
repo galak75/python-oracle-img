0938640d8cfb70ad6ad67efbc50e0cfc1fbc5bc0
Géraud Lac <geraud.lac@montreal.ca>
2023-10-22 21:19:57 -0400

    force image architecture to amd64

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "*** Building image..."
-docker build -t my-image .
+docker build -t my-image . --platform amd64
 echo "*** Image successfully built."
 echo
 echo "*****************************"
